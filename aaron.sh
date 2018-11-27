fbc80bec16f22dcac77bfc08fb289729912517f1
aaron <aaronchongth@gmail.com>
2018-11-27 18:19:38 -0500

    first commit

---
+++ aaron.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git config --global user.name "aaronchongth"
+git config --global user.email aaronchongth@gmail.com
---
+++ dje.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git config --global user.name "davidjevans"
+git config --global user.email dezeumer@gmail.com
