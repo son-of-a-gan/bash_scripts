e62a805e5cd4266d38d6c7bfcedfa253f3dcb7e2
davidjevans <dezeumer@gmail.com>
2018-11-27 18:28:08 -0500

    git scripts for adam and david 1

---
+++ adam.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git config --global user.name "jdriscoll319"
+git config --global user.email jdriscol@andrew.cmu.edu
---
+++ drob.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git config --global user.name "davidirobinson"
+git config --global user.email dvsrobinson@gmail.com
